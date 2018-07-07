c6b90d53477d9d47b2df948e620de61f11b483c9
Nicholas Manning <nicholas.manning@gmail.com>
2018-07-07 10:24:51 -0400

    More tests for non-verbose mode. Finished the README.md

--- README.md
+++ README.md
@@ -1,8 +1,22 @@
 # pure_mixpanel
 
-A new flutter package project.
+A pure Dart library for [Mixpanel analytics](https://www.mixpanel.com). 
+
+## Running Tests
+
+This library has [Flutter](http://flutter.io) as a dependency for easier testability. The tests are true end to end tests, using a real Mixpanel account and API token that anyone is free to use. I decided against mocking the API for now to keep the codebase simple.
+
+```
+flutter test test/pure_mixpanel_test.dart
+```
+
+## Features
+
+- 7/7/18: event tracking
 
 ## TODO
 
-- [ ] README
-- [ ] Flutter dependency required?
+Feel free to contribute!
+
+- [ ] storing [User Profiles](https://mixpanel.com/help/reference/http)
+- [ ] testing of invalid requests
--- test/pure_mixpanel_test.dart
+++ test/pure_mixpanel_test.dart
@@ -3,23 +3,49 @@ import 'dart:convert';
 import 'package:pure_mixpanel/pure_mixpanel.dart';
 
 void main() {
-  test('adds one to input values', () async {
+  test('Mixpanel track events', () async {
+    const pureMixpanelPublicAPIToken = 'f9b25358a89f5a20adaecc608a7d6f72';
     const cases = [
       {
         'event': 'foobar',
         'distinct_id': '492',
-        'token': '7efab866c097943d3a2273c0adac028f',
+        'token': pureMixpanelPublicAPIToken,
         'debug': true,
-        'expects': true
+        'expects_status_code': 200,
+        'expects_status': 1,
+      },
+      {
+        'event': 'foobar',
+        'distinct_id': null,
+        'token': pureMixpanelPublicAPIToken,
+        'debug': false,
+        'expects_status_code': 200,
+        'expects_status': 1,
       },
     ];
 
     // NOTE for some reason, loop cases above using 'async' doesn't work
     // so doing this manually
-    var mixpanel = Mixpanel(token: cases[0]['token'], debug: cases[0]['debug']);
-    var result = await mixpanel.track(cases[0]['event'],
-        distinctID: cases[0]['distinct_id']);
-    print('>>> result is ${result}');
-    expect(cases[0]['expects'], (result.statusCode == 200));
+    for (var i = 0; i < cases.length; i++) {
+      print('testing case ${cases[i]}');
+      var k = cases[i];
+      var mixpanel = Mixpanel(token: k['token'], debug: k['debug']);
+      var result =
+          await mixpanel.track(k['event'], distinctID: k['distinct_id']);
+      if (k['debug'] == true) {
+        // if in debug mode, lib adds a 'verbose' flag and Mixpanel returns a
+        // JSON object
+        Map<String, dynamic> respMap = json.decode(result.body);
+        print('got response $respMap');
+        expect(k['expects_status'], respMap['status']);
+      } else {
+        // normally, Mixpanel returns a single integer representing success or not
+        int respStatus = json.decode(result.body);
+        print('got response $respStatus');
+        expect(k['expects_status'], respStatus);
+      }
+      // test for standard HTTP response status code
+      expect(k['expects_status_code'], result.statusCode);
+    }
   });
 }
